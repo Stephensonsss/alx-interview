51e253272c1b0fcf2417244faa1cc9e295d85313
stephensonsss <stephensonsss3@mail.com>
2024-11-16 14:33:03 +0100

    0x06-starwars_api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        console.error('Error:', response.statusCode);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
+
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,9 @@
+# Star Wars Characters
+
+This helps to script prints all characters of a Star Wars movie.
+
+## Usage
+
+```sh
+./0-starwars_characters.js <Movie ID>
+
