626e7083676b3fbfb9039fc7bd6d41be5d1416e2
stephensonsss <stephensonsss3@mail.com>
2024-11-28 11:48:55 +0100

    0-making_change

---
+++ 0x08-making_change/0-main.py
@@ -0,0 +1,15 @@
+carrie@ubuntu:~/0x08-making_change$ cat 0-main.py
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))  # Output: 7
+print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
+
+carrie@ubuntu:~/0x08-making_change$ ./0-main.py
+7
+-1
+
